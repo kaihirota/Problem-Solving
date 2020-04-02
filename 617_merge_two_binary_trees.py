0b1aef5e645f8dae80979cb39ae9f5ad86d6ff39
Kai Hirota <kai@khirota.co>
2020-04-02 17:15:48 +1100

    modified .gitignore to include everything in working directory

--- 617_merge_two_binary_trees.py
+++ 617_merge_two_binary_trees.py
@@ -1,3 +1,36 @@
+"""
+https://leetcode.com/problems/merge-two-binary-trees/
+
+617. Merge Two Binary Trees
+
+Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
+
+You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
+
+Example 1:
+
+Input:
+	Tree 1                     Tree 2
+          1                         2
+         / \                       / \
+        3   2                     1   3
+       /                           \   \
+      5                             4   7
+Output:
+Merged tree:
+	     3
+	    / \
+	   4   5
+	  / \   \
+	 5   4   7
+
+
+Note: The merging process must start from the root nodes of both trees.
+
+reflection:
+After taking a break, I was able to revise my code line by line and slowly but precisely identify what will go wrong. I did this by visualizing edge cases and running mental simulation.
+"""
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
@@ -29,35 +62,3 @@ class Solution:
                 merged.right = t1.right
 
         return merged
-
-"""
-https://leetcode.com/problems/merge-two-binary-trees/
-617. Merge Two Binary Trees
-
-Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
-
-You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
-
-Example 1:
-
-Input:
-	Tree 1                     Tree 2
-          1                         2
-         / \                       / \
-        3   2                     1   3
-       /                           \   \
-      5                             4   7
-Output:
-Merged tree:
-	     3
-	    / \
-	   4   5
-	  / \   \
-	 5   4   7
-
-
-Note: The merging process must start from the root nodes of both trees.
-
-reflection:
-After taking a break, I was able to revise my code line by line and slowly but precisely identify what will go wrong. I did this by visualizing edge cases and running mental simulation.
-"""
