688c3311da869fdbf8316a52f5b389923ee95057
Kai Hirota <kai@khirota.co>
2020-04-03 19:44:36 +1100

    finished maximum subarray

---
+++ 53_maximum_subarray.py
@@ -0,0 +1,31 @@
+"""
+https://leetcode.com/problems/maximum-subarray/
+
+53. Maximum Subarray
+
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Follow up:
+
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+solution: Kadane's Algorithm
+"""
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_max = nums[0]
+        global_max = nums[0]
+
+        for i in range(1, len(nums)):
+            current_max = max(nums[i], nums[i] + current_max)
+
+            if current_max > global_max:
+                global_max = current_max
+
+        return global_max
