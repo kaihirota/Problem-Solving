674a2c3dcd01d0ab0263f0d999e958409eb88785
Kai Hirota <kai@khirota.co>
2020-05-04 20:07:59 +1000

    update

--- algorithms/oop.py
+++ algorithms/oop.py
@@ -1,14 +1,14 @@
 import numpy as np
 import pandas as pd
 
-class NeoDataFrame(pd.DataFrame):
+class DataFrame2(pd.DataFrame):
     def __init__(self, data):
         super().__init__(data=data)
 
-    def power_up(self):
-        print('POWERRRRRRRRRRR')
+    def do_something(self):
+        print('do_something')
 
 
 x = np.random.normal(size=300)
-df = NeoDataFrame(x.reshape((60, 5)))
+df = DataFrame2(x.reshape((60, 5)))
 print(type(df))
