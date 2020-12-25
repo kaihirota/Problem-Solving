3fddc375e58b01a2575f8ce8f763c0f03a9226f3
Kai Hirota <kai@wardrobeus.com>
2020-12-25 20:45:42 +1100

    added foobar challenge

--- algorithms/dynamic_programming/coin_change.py
+++ algorithms/dynamic_programming/coin_change.py
@@ -14,6 +14,24 @@ class Solution(object):
         return dp[-1]
 
 
+from functools import lru_cache
+
+class Solution:
+    def mincostTickets(self, days, costs):
+        dayset = set(days)
+        durations = [1, 7, 30]
+
+        @lru_cache(None)
+        def dp(i):
+            if i > 365:
+                return 0
+            elif i in dayset:
+                return min(dp(i + d) + c
+                           for c, d in zip(costs, durations))
+            else:
+                return dp(i + 1)
+
+        return dp(1)
 coins = [1, 2, 5]
 amount = 11
 assert Solution().coinChange(coins, amount) == 3
